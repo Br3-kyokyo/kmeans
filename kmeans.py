fe3ccda45c0693726b7367a69bffc5f70f41f40a
KyoheiSasaki <danibusters.main@gmail.com>
2019-01-09 20:04:42 +0900

    initialize repos.

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,19 @@
+# パターン認識プログラミング課題 K-means法
+# s1611133 佐々木恭平 
+#  
+# 時々、クラスタの数が0になってZeroDivisionErrorがおきます、すみません
+# CLUSTERNUMの値を変更すればクラスタ数を変更できるようになっています 
+#
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import ffmpeg
 from mpl_toolkits.mplot3d import Axes3D 
 
-CLUSTERNUM = 3
+#クラスタ数指定
+CLUSTERNUM = 4
 
+#関数宣言
 def abs(v1, v2):
     return np.sqrt((v1[0]-v2[0])**2 + (v1[1]-v2[1])**2 + (v1[1]-v2[1])**2)
 
@@ -20,17 +28,11 @@ def mean(result, cluster):
             z_sum += result[n, 2]
             x_num += 1            
             y_num += 1
-            z_num += 1
-    
+            z_num += 1    
     
     x_mean = (x_sum/x_num)
     y_mean = (y_sum/y_num)
     z_mean = (z_sum/z_num)
-    
-    #rint("mean")
-    #rint(x_mean)
-    #rint(y_mean)
-    #int(z_mean)
     
     return np.array([x_mean, y_mean, z_mean, 0])
 
@@ -47,70 +49,67 @@ def select_row(result, cluster):
     return resultArray
 
 
+##以下メイン部##
+
+#データ読み込み
 data = pd.read_csv("./kmeansdata", header=None).values
-
+#各データにクラスタ番号を紐付け
 result = np.insert(data, 3, -1, axis=1)
+#セントロイドの初期値をランダムに決定(データの座標を拝借)
 cent_init = np.random.choice(np.arange(0,30), CLUSTERNUM)
-
-centroid = [result[cent_init[0]], result[cent_init[1]], result[cent_init[2]]]
+centroid = []
+for cluster in np.arange(0, CLUSTERNUM):
+    centroid.append(result[cent_init[cluster]])
 centroid = np.array(centroid)
 
-
+#メインループ
 while True:    
-    endflag = True
+    endflag = True #クラスタ番号の変更を検知したらendflagがFalseになる
+    
+    #全データについてクラスタ番号を決定
     for n in np.arange(0,30):
         
         centroid_prev = result[n][3]
-        
-        dest0=abs(result[n], centroid[0])
-        dest1=abs(result[n], centroid[1])
-        dest2=abs(result[n], centroid[2])
 
-        if(dest0 < dest1 and dest0 < dest2):
-            result[n][3] = 0
-        elif(dest1 < dest0 and dest1 < dest2):
-            result[n][3] = 1
-        else:
-            result[n][3] = 2
-
+        #各セントロイドとの距離を算出
+        dest = []
+        for cluster in np.arange(0, CLUSTERNUM):
+            dest.append(abs(result[n], centroid[cluster]))
+        dest = np.array(dest)
+       
+        #最も近いセントロイドを選出しセントロイド番号を更新
+        for cluster in np.arange(0, CLUSTERNUM):
+            if(min(dest) == dest[cluster]):
+                result[n][3] = cluster           
+            
         if(centroid_prev != result[n][3]):
             endflag = False
             
     if(endflag):
-        break
-        
-    centroid[0] = mean(result, 0)
-    centroid[1] = mean(result, 1)
-    centroid[2] = mean(result, 2)
+        break 
     
-    print(centroid)
-        
+    #セントロイド更新
+    for cluster in np.arange(0, CLUSTERNUM):
+        centroid[cluster] = mean(result, cluster)
+    
 
+
+##以下描画フェーズ##
 fig = plt.figure()
 scatGraph = fig.add_subplot(1,1,1,projection='3d')
 
-result_0 = select_row(result, 0)
-result_1 = select_row(result, 1)
-result_2 = select_row(result, 2)
+clustercol = ['red', 'green', 'yellow', 'blue', 'black', 'magenta']
 
-x=result_0[:,0]
-y=result_0[:,1]
-z=result_0[:,2]
-scatGraph.scatter(x, y, z, edgecolors='red', alpha=1)
-
-x=result_1[:,0]
-y=result_1[:,1]
-z=result_1[:,2]
-scatGraph.scatter(x, y, z, edgecolors='green', alpha=1)
-
-x=result_2[:,0]
-y=result_2[:,1]
-z=result_2[:,2]
-scatGraph.scatter(x, y, z, edgecolors='yellow', alpha=1)
+for cluster in np.arange(0, CLUSTERNUM):
+    x = []
+    result_axis = np.array(select_row(result, cluster))
+    for axis in np.arange(0, 3):
+        x.append(result_axis[:,axis])
+    x = np.array(x)
+    scatGraph.scatter(x[0], x[1], x[2], edgecolors=clustercol[cluster], alpha=1)
 
 scatGraph.set_xlabel('GDP/person')
 scatGraph.set_ylabel('Population Density')
 scatGraph.set_zlabel('Birthrate')
 
 plt.show()
-
